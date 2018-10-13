c1e49f9a5183fb90994166ddb7d8c7899f862bbf
Javin <jtwong@ualberta.ca>
2018-10-12 21:37:30 -0600

    Lab 5 Commit

---
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivity.java
@@ -0,0 +1,95 @@
+package ca.ualberta.cs.lonelytwitter;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.Date;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+public class LonelyTwitterActivity extends Activity {
+
+	private static final String FILENAME = "file.sav";
+	private EditText bodyText;
+	private ListView oldTweetsList;
+	private ArrayList<Tweet> tweetList = new ArrayList<Tweet>();
+	private ArrayAdapter<Tweet> adapter;
+
+
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		bodyText = (EditText) findViewById(R.id.body);
+		Button saveButton = (Button) findViewById(R.id.save);
+		Button searchButton = (Button) findViewById(R.id.search);
+		oldTweetsList = (ListView) findViewById(R.id.oldTweetsList);
+
+		saveButton.setOnClickListener(new View.OnClickListener() {
+
+			public void onClick(View v) {
+				setResult(RESULT_OK);
+				String text = bodyText.getText().toString();
+				Tweet newTweet = new NormalTweet(text);
+				tweetList.add(newTweet);
+				adapter.notifyDataSetChanged();
+				new ElasticsearchTweetController
+						.AddTweetTask().execute(newTweet);
+
+
+			}
+		});
+
+		searchButton.setOnClickListener(new View.OnClickListener() {
+
+			public void onClick(View v) {
+				setResult(RESULT_OK);
+				String text = bodyText.getText().toString();
+				Tweet newTweet = new NormalTweet(text);
+				adapter.notifyDataSetChanged();
+				new ElasticsearchTweetController
+						.GetTweetsTask().execute(newTweet);
+			}
+		});
+
+
+	}
+
+	@Override
+	protected void onStart() {
+		// TODO Auto-generated method stub
+		super.onStart();
+		try {
+            tweetList = new ElasticsearchTweetController.GetTweetsTask().execute("").get();
+        }catch(Exception e){}// this is bad.. don't do this
+		adapter = new ArrayAdapter<Tweet>(this,
+				R.layout.list_item, tweetList);
+		oldTweetsList.setAdapter(adapter);
+	}
+
+
+
+	}
+
+
+
